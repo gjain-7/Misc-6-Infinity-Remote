e884d158f1c14b5ffb7e076847bf0981b24942d9
Mitternachtsmond <85838936+Mitternachtsmond@users.noreply.github.com>
2021-08-02 01:50:32 +0530

    Add files via upload

--- Frontend/popup.js
+++ Frontend/popup.js
@@ -1,4 +1,27 @@
-console.log("Hi this is popup.js")
+// console.log("Hi this is popup.js")
+// function createqr(info) {
+//   var qr = new QRious({
+//     element: document.getElementById("qr-code"),
+//     size: 250,
+//     value: info,
+//   });
+// }
+// // createqr("http://localhost:5500/extensionss/mainpage.html");
+
+// chrome.runtime.onMessage.addListener(gotMessage);
+// function gotMessage(message, sender, sendResponse) {
+//   console.log(message);
+// }
+
+// function updatePopup() {
+//   chrome.storage.local.get(['roomName'], function (data) {
+//       createqr(data.roomName)
+//   })
+// }
+
+// updatePopup()
+
+console.log("Popup clicked")
 function createqr(info) {
   var qr = new QRious({
     element: document.getElementById("qr-code"),
@@ -6,7 +29,6 @@ function createqr(info) {
     value: info,
   });
 }
-// createqr("http://localhost:5500/extensionss/mainpage.html");
 
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendResponse) {
@@ -20,3 +42,14 @@ function updatePopup() {
 }
 
 updatePopup()
+var instruction = document.querySelector("p").innerHTML
+document.querySelector("a").onclick(function(){
+  instruction = "Scan this QR-Code with a <a></a> camera";
+  createqr("something");
+  document.querySelector("button").style.display="block"
+})
+
+document.querySelector("button").onclick(function(){
+  instruction= "Scan this QR-code with the <a>Infinity Remote </a>website."
+  updatePopup();
+})
