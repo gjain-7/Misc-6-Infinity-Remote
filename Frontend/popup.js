77405f98a22588fc02945a87658e8acad13893ea
mukulvain <79629333+mukulvain@users.noreply.github.com>
2021-08-02 02:42:42 +0530

    Add files via upload

--- Frontend/popup.js
+++ Frontend/popup.js
@@ -1,27 +1,4 @@
-// console.log("Hi this is popup.js")
-// function createqr(info) {
-//   var qr = new QRious({
-//     element: document.getElementById("qr-code"),
-//     size: 250,
-//     value: info,
-//   });
-// }
-// // createqr("http://localhost:5500/extensionss/mainpage.html");
-
-// chrome.runtime.onMessage.addListener(gotMessage);
-// function gotMessage(message, sender, sendResponse) {
-//   console.log(message);
-// }
-
-// function updatePopup() {
-//   chrome.storage.local.get(['roomName'], function (data) {
-//       createqr(data.roomName)
-//   })
-// }
-
-// updatePopup()
-
-console.log("Popup clicked")
+console.log("Popup clicked");
 function createqr(info) {
   var qr = new QRious({
     element: document.getElementById("qr-code"),
@@ -30,26 +7,26 @@ function createqr(info) {
   });
 }
 
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message, sender, sendResponse) {
-  console.log(message);
-}
-
 function updatePopup() {
-  chrome.storage.local.get(['roomName'], function (data) {
-      createqr(data.roomName)
-  })
+  chrome.storage.local.get(["roomName"], function (data) {
+    createqr(data.roomName);
+  });
 }
 
-updatePopup()
-var instruction = document.querySelector("p").innerHTML
-document.querySelector("a").onclick(function(){
-  instruction = "Scan this QR-Code with a <a></a> camera";
-  createqr("something");
-  document.querySelector("button").style.display="block"
-})
+updatePopup();
 
-document.querySelector("button").onclick(function(){
-  instruction= "Scan this QR-code with the <a>Infinity Remote </a>website."
+var instruction = document.querySelector("p");
+document.querySelector("a").addEventListener("click", QRcodeofSite);
+function QRcodeofSite() {
+  instruction.innerHTML = "Scan this QR-Code with your mobile's<a></a> camera";
+  createqr("https://www.something something.herokuapp.com");
+  document.querySelector("button").style.visibility = "visible";
+}
+document.querySelector("button").addEventListener("click",QRcodeofExtension);
+function QRcodeofExtension() {
+  instruction.innerHTML =
+    "Scan this QR-code with the <a>Infinity Remote</a> website.";
+    document.querySelector("button").style.visibility= "hidden";
   updatePopup();
-})
+  document.querySelector("a").addEventListener("click", QRcodeofSite);
+}
