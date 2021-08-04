1f685425a40f8c5649cebd4e4e5a85fbff3a68cc
mukulvain <79629333+mukulvain@users.noreply.github.com>
2021-08-04 12:38:08 +0530

    Update sw.js

--- Backend/sw.js
+++ Backend/sw.js
@@ -1,7 +1,23 @@
 const staticCache = "static-v1"
 const assets = [
-    "/", "/server.js","/views/connecting.ejs","/views/mainpage.ejs","/views/index.ejs",
-    "/public/mainpage.css","/public/welcome.css","/public/welcome.js","/public/connecting.css","/public/mainpage.js","/public/css/fontawesome.min.css","/public/css/all.min.css", "/public/icons/icon.svg", "https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js", "/views/offline.ejs","/views/invalid.ejs","/views/error.ejs","/public/icons/error.png","/public/error.css"
+    "/", "/server.js",
+    "/views/connecting.ejs",
+    "/views/mainpage.ejs",
+    "/views/index.ejs",
+    "/public/mainpage.css",
+    "/public/welcome.css",
+    "/public/welcome.js",
+    "/public/connecting.css",
+    "/public/mainpage.js",
+    "/public/css/fontawesome.min.css",
+    "/public/css/all.min.css",
+    "/public/icons/icon.svg", 
+    "https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js", 
+    "/views/offline.ejs",
+    "/views/invalid.ejs",
+    "/views/error.ejs",
+    "/public/icons/error.png",
+    "/public/error.css"
 ]
 self.addEventListener('install', function(event){
     console.log("Service Worker Installed")
