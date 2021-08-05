8ad65686f5f4db96d3ecb35068341d04e3043e42
mukulvain <79629333+mukulvain@users.noreply.github.com>
2021-08-05 16:40:00 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,38 @@
+const compression = require('compression');
 const express = require("express");
 const { SocketAddress } = require("net");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: ["https://infinity-remote.herokuapp.com/*","http://infinity-remote.herokuapp.com/*"],
   },
 });
 
+const path =__dirname;
 app.set("views", "./views");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(compression());
+app.use(express.static(path));
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  else
+    next()
+})
 
 app.get("/", (req, res) => {
   res.render("welcome");
 });
 app.get("/qr-scanner", (req, res) => {
-  res.render("index" /*,{ rooms: rooms } */); //scanner nishit
+  res.render("index");
 });
 
 app.get("/invalid", (req, res) => {
   res.render("invalid");
 });
+
 app.get("/error", (req, res) => {
   res.render("error");
 });
@@ -33,15 +43,22 @@ app.get("/mainpage", (req, res) => {
   res.render("geterror");
 });
 
+app.get("/sitemap.xml", (req, res) =>{
+  res.contentType("application/xml")
+  res.sendFile("/sitemap.xml")
+})
+
 app.post("/connecting", (req, res) => {
   res.render("connecting", {roomName: req.body.room});
 });
 
 app.post("/mainpage", (req, res) => {
-  res.render("mainpage", {roomName: req.body.room}); // where "room" is the name of the roomName acquired from the qr-code scanner(nishit's)
+  res.render("mainpage", {roomName: req.body.room}); // where "room" is the name of the roomName acquired from the qr-code scanner
   console.log("redirecting user to mainpage.html ...");
 });
-
+app.all("*", function (req, res) {
+  res.redirect("https://infinity-remote.herokuapp.com");
+});
 server.listen(process.env.PORT || 3000);
 
 io.on("connection", (socket) => {
@@ -50,8 +67,8 @@ io.on("connection", (socket) => {
   socket.on("new-pwa-user", (room) => {
     // joining popup
     console.log(socket.id);
-    if (io.sockets.adapter.rooms.has(room)){
-      let roomie = io.sockets.adapter.rooms.get(room);
+    let roomie = io.sockets.adapter.rooms.get(room);
+    if (roomie){
       console.log(roomie, " ", roomie.size);
       if (roomie.size < 2) {
         socket.join(room);
@@ -65,21 +82,20 @@ io.on("connection", (socket) => {
     else{
       io.in(socket.id).emit("Connection Error");
     }
-
   });
 
   socket.on("popupmessage", (message, room) => {
-    // listening popup and sending content
+    // listening to popup and sending to content
     console.log("pwa: ", message);
     console.log(socket.id, " room: ", socket.rooms);
     let roomie = io.sockets.adapter.rooms.get(room);
-    if (roomie.has(socket.id)) {
-      socket.to(room).emit("popupmessage", message);
+    if (roomie && roomie.has(socket.id)) {
+      io.in(room).emit("popupmessage", message);
     }
   });
 
   socket.on("contentmessage", (message) => {
-    // listening content and sending popup
+    // listening to content and sending to popup
     console.log("extension: ", message);
     console.log(socket.id, " room: ", socket.rooms);
     io.in(socket.id).emit("contentmessage", message);
--- Backend/sw.js
+++ Backend/sw.js
@@ -1,65 +1,38 @@
-const staticCache = "static-v1"
+const staticCache = "static-v1";
 const assets = [
-    "/", "/server.js",
-    "/views/connecting.ejs",
-    "/views/mainpage.ejs",
-    "/views/index.ejs",
-    "/public/mainpage.css",
-    "/public/welcome.css",
-    "/public/welcome.js",
-    "/public/connecting.css",
-    "/public/mainpage.js",
-    "/public/css/fontawesome.min.css",
-    "/public/css/all.min.css",
-    "/public/icons/icon.svg", 
-    "https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js", 
-    "/views/offline.ejs",
-    "/views/invalid.ejs",
-    "/views/error.ejs",
-    "/public/icons/error.png",
-    "/public/error.css"
-]
-self.addEventListener('install', function(event){
-    console.log("Service Worker Installed")
-    event.waitUntil(
-        caches.open(staticCache).then(function(cache){
-            console.log("Caching Assets")
-            return cache.addAll(assets);
-        })
-    );
+  "/",
+  "/views/offline.html",
+  "/public/error.css",
+  "/public/icons/error.png",
+];
+self.addEventListener("install", function (event) {
+  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(staticCache).then(function (cache) {
+      console.log("Caching Assets");
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener("activate", function(event){
-    event.waitUntil(
-        caches.keys().then(keys =>{
-            console.log(keys);
-            return Promise.all(keys
-                .filter(key => key ==! staticCache)
-                .map(key => caches.delete())
-            )
-        })
-    )
-})
-self.addEventListener('fetch', function(event){
-    const options = {
-    ignoreVary: true
-  };
-    console.log("Fetch Event", event);
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            return response || fetch(event.request).catch(()=> caches.match("/views/index.ejs"));
-        })
-    );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      console.log(keys);
+      return Promise.all(
+        keys.filter((key) => key !== staticCache).map((key) => caches.delete())
+      );
+    })
+  );
 });
-self.addEventListener('fetch', function(event){
-    const options = {
-    ignoreVary: true
-  };
-    console.log("Fetch Event", event);
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            return response || fetch(event.request).catch(()=> caches.match("/views/offline.ejs"));
-        })
-    );
+self.addEventListener("fetch", function (event) {
+  console.log("Fetch Event", event);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("/views/offline.html"))
+      );
+    })
+  );
 });
-
